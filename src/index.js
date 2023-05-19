0222947c88b494543a71505060719635c13b757e
gyonungi <104500843+gyonungi@users.noreply.github.com>
2023-05-19 17:25:50 +0700

    change useState sort

--- src/index.js
+++ src/index.js
@@ -3,15 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "./styles/main.css";
-import { Provider } from "react-redux";
-import { Store } from "./store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-      <Provider store={Store}>
     <BrowserRouter>
         <App />
     </BrowserRouter>
-      </Provider>
   </React.StrictMode>
 );
