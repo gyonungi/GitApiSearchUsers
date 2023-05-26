e1cc77a5a38cc28ab5c68a53e16a00bc9682c376
gyonungi <104500843+gyonungi@users.noreply.github.com>
2023-05-26 18:03:55 +0700

    Add files via upload
    changeMain

--- src/pages/MainPage/Main.jsx
+++ src/pages/MainPage/Main.jsx
@@ -1,78 +1,82 @@
 import { useEffect, useState } from "react";
 import s from "./Main.module.css";
 import Card from "../../components/Card/Card";
-import ReactPaginate from "react-paginate";
 const Main = () => {
   const [inputValue, setInputValue] = useState("");
-  const [isSorted, setisSorted] = useState(false);
   const [users, setUsers] = useState([]);
-
-  const handleClick = () => {
-    setisSorted((current) => !current);
-  };
-  /*   const dispatch = useDispatch(); */
-  const getUsers = async (value) => {
+  
+  const getUsers = async (value,page) => {
     const response = await fetch(
-      `https://api.github.com/search/users?q=${value}`,
-      {headers: {'Content-Type': 'application/json'}, method: "GET"}
-  );
+      `https://api.github.com/search/users?q=${value}&sort=repositories&order=asc&page=${page}`,
+      { headers: { "Content-Type": "application/json" },
+       method: "GET" }
+    );
     const users = await response.json();
     setUsers(users);
   };
   useEffect(() => {
     getUsers();
   }, []);
-
-  /*   function searchGit(e) {
-    e.preventDefault();
-    dispatch(getUsers(inputValue));
-  } */
   //INPUT
   const handleInputChange = (event) => {
     let inputValue = event.target.value;
     setInputValue(inputValue);
     getUsers(inputValue);
   };
-/*   console.log(gitUser?.[2]?.items); */
   console.log(users?.items);
-
+  console.log(users?.total_count);
   //SORT
   const [sortUser, setSortUser] = useState(null);
-  function numAscending() {
+  function numDescending() {
     users?.items?.sort((a, b) => a?.repos_url?.length - b?.repos_url?.length);
-  return  setSortUser(sortUser + users?.items);
+    return setSortUser(sortUser + users);
   }
   const [sortUsers, setSortUsers] = useState(null);
-  function numDescending() {  
-    users?.items?.sort(
-      (a, b) => b?.repos_url?.length - a?.repos_url?.length
-    );
-    return  setSortUsers(sortUsers + users?.items);
+  function numAscending() {
+    users?.items?.sort((a, b) => b?.repos_url?.length - a?.repos_url?.length);
+    return setSortUsers(sortUsers + users);
   }
 
   //PAGINATINO
-  const [page, setPage] = useState(0);
-  const usersPerPage = 5;
-  const numberOfusersVistited = page * usersPerPage;
-  const displayusers = users?.items?.slice(numberOfusersVistited, numberOfusersVistited + usersPerPage)  
-    .map((item) => {
+  const [currentPage,setCurrentPage] = useState(1)
+  console.log(currentPage);
+  const todosPerPage = 30;
+  const indexOfLastTodo = currentPage * todosPerPage;
+  const indexOfFirstTodo = indexOfLastTodo - todosPerPage;
+  const displayusers = users?.items
+    ?.slice(indexOfFirstTodo, indexOfLastTodo)
+    ?.map((item) => {
       return <Card key={item.id} date={item} />;
     });
-  const totalPages = Math.ceil(users?.items?.length / usersPerPage);
-  const changePage = ({ selected }) => {
-    setPage(selected);
+  const renderTodos = displayusers?.map((todo, index) => {
+    return <li key={index}>{todo}</li>;
+  });
+  const clickPagin = (e) => {
+    setCurrentPage(Number(e.target.id));
   };
+
+  // Logic for displaying page numbers
+  const pageNumbers = [];
+  for (let i = 1; i <= Math.ceil(users?.total_count / todosPerPage); i++) {
+    pageNumbers.push(i);
+  }
+  const renderPageNumbers = pageNumbers?.map((number) => {
+    return (
+      <li key={number} id={number} onClick={(e) => clickPagin(e)}>
+        {number}
+      </li>
+    );
+  }); 
   return (
     <>
       <h2 className={s.mainH2}>GitHub Search 🔍</h2>
       <div className={s.mainSearch}>
         <form
-          /* onSubmit={(e) => searchGit(e)} */
           className={s.searchForm}
           action="#"
         >
           <input
-          data-testid="value-elem"
+            data-testid="value-elem"
             className={s.searchText}
             placeholder="Поиск пользователя"
             name="search"
@@ -82,26 +86,28 @@ const Main = () => {
           />
         </form>
       </div>
-      <div className={s.DivSort}> 
-       <p>Сортировка:</p> 
-       <div>
-      <button data-testid="Asc-btn" className={s.Asc} onClick={numAscending}>Вниз</button>
-      <button data-testid="Desc-btn" className={s.Desc}onClick={numDescending}>Вверх</button>
-      </div>
+      <ul className={s.pagenumbers}>{renderPageNumbers}</ul> 
+      <div className={s.DivSort}>
+        <p>Сортировка:</p>
+        <div>
+          <button
+            data-testid="Asc-btn"
+            className={s.Asc}
+            onClick={numAscending}
+          >
+            Вниз
+          </button>
+          <button
+            data-testid="Desc-btn"
+            className={s.Desc}
+            onClick={numDescending}
+          >
+            Вверх
+          </button>
+        </div>
       </div>
       <div className={s.DivCards}>
-        {users ? (displayusers): (<p>Не найден пользователь</p>) }
-        <ReactPaginate
-          previousLabel={"Previous"}
-          nextLabel={"Next"}
-          pageCount={totalPages}
-          onPageChange={changePage}
-          containerClassName={s.navigationButtons}
-          previousLinkClassName={s.previousButton}
-          nextLinkClassName={s.nextButton}
-          disabledClassName={s.navigationDisabled}
-          activeClassName={s.navigationActive}
-        />
+        {users ? (renderTodos): (<p>Не найден пользователь</p>) }
       </div>
     </>
   );
